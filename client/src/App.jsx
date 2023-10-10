cb0517b3c8ce2029d32427d0e7c58a4f01e62b93
sandking-number1 <Alfred.J9412@gmail.com>
2023-10-10 19:29:58 +0200

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
-import Header from './pages/Header'
+import Header from './components/Header'
 
 const App = () => {
   return (
